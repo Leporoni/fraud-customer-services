ea7ff0a817125f32818facec04f6aec91da5b914
Leporoni <leporoni@hotmail.com>
2022-01-13 16:38:44 -0300

    Created log for FraudControler

--- fraud/src/main/java/com/leporonitech/fraud/FraudControler.java
+++ fraud/src/main/java/com/leporonitech/fraud/FraudControler.java
@@ -1,11 +1,16 @@
 package com.leporonitech.fraud;
 
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/v1/fraud-check")
 @AllArgsConstructor
+@Slf4j
 public class FraudControler {
 
     private final FraudCheckService fraudCheckService;
@@ -14,6 +19,8 @@ public class FraudControler {
     public FraudCheckResponse isFraudster(@PathVariable("customerId") Integer customerId) {
 
         boolean isFraudulentCustomer = fraudCheckService.isFraudulentCustomer(customerId);
+        log.info("fraud check request for customer {}", customerId);
+
         return new FraudCheckResponse(isFraudulentCustomer);
     }
 }
